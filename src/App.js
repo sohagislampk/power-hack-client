7a9149078b2fa8969fbf9edf1cbfef27acd1c2e8
sohagislampk <sohagislampk@gmail.com>
2023-01-29 14:38:06 +0600

    Navbar and Layout setup

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
 
+import { RouterProvider } from 'react-router-dom';
 import './App.css';
+import routes from './Routes/Routes/Routes';
 
 function App() {
   return (
     <div className="App">
-      <button className="btn">Button</button>
-      <button className="btn btn-primary">Button</button>
-      <button className="btn btn-secondary">Button</button>
-      <button className="btn btn-accent">Button</button>
-      <button className="btn btn-ghost">Button</button>
-      <button className="btn btn-link">Button</button>
+      <RouterProvider router={routes} ></RouterProvider>
     </div>
   );
 }
---
+++ src/Routes/Routes/Routes.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { createBrowserRouter } from 'react-router-dom';
+import Main from '../../Layouts/Main/Main';
+import Home from '../../Pages/Home/Home/Home';
+
+const routes = createBrowserRouter([
+    {
+        path: '/',
+        element: <Main></Main>,
+        children: [{
+            path: '/',
+            element: <Home></Home>
+        }]
+    }
+])
+export default routes;
