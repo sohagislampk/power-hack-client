517c64b2dda713d5622f60a676778d2266c08068
sohagislampk <sohagislampk@gmail.com>
2023-01-29 18:00:36 +0600

    Login and Register page desing

--- src/Pages/Shared/Navbar/Navbar.js
+++ src/Pages/Shared/Navbar/Navbar.js
@@ -1,15 +1,19 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
     return (
-        <div className="navbar bg-base-100">
+        <div className="navbar bg-accent">
             <div className="flex-1">
-                <a className="btn btn-ghost normal-case text-xl">daisyUI</a>
+                <Link to={'/'} className="btn btn-ghost normal-case text-xl">Power-Hack</Link>
             </div>
             <div className="flex-none">
-                <button className="btn btn-square btn-ghost">
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-5 h-5 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z"></path></svg>
-                </button>
+                <Link to={'/login'}>  <button className="btn btn-outline btn-secondary mr-5">
+                    Login
+                </button></Link>
+                <Link to={'/register'}> <button className="btn btn-outline btn-secondary">
+                    Register
+                </button></Link>
             </div>
         </div>
     );
--- src/Routes/Routes/Routes.js
+++ src/Routes/Routes/Routes.js
@@ -2,15 +2,28 @@ import React from 'react';
 import { createBrowserRouter } from 'react-router-dom';
 import Main from '../../Layouts/Main/Main';
 import Home from '../../Pages/Home/Home/Home';
+import Login from '../../Pages/Login/Login';
+import Register from '../../Pages/Register/Register';
 
 const routes = createBrowserRouter([
     {
         path: '/',
         element: <Main></Main>,
-        children: [{
-            path: '/',
-            element: <Home></Home>
-        }]
+        children: [
+            {
+                path: '/',
+                element: <Home></Home>
+            },
+            {
+                path: '/login',
+                element: <Login></Login>
+            },
+            {
+                path: '/register',
+                element: <Register></Register>
+            },
+
+        ]
     }
 ])
 export default routes;
